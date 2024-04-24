217795fbd7f309d8db7411f13e3df4dcb7f50360
carusi99 <paolapachecocarusi@gmail.com>
2024-04-23 19:15:40 -0500

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ShoppingCartApp from "./ShoppingCartApp";
+
+
+function App() {
+  return (
+    <main>
+      <div className="section">
+        <div className="container">
+          <div className="article-container">
+            <article className="article">
+              <div className="content">
+                <ShoppingCartApp />
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
